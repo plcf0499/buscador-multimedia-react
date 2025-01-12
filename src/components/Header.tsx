aa5fce63bb1ec64aa3c130f8b7d2f3b6cd384f66
plcf0499 <plcf0499@gmail.com>
2025-01-12 16:33:39 -0500

    v4. Página previa al buscador agregada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => (
   <header style={{ padding: "1rem", backgroundColor: "#000", color: "#fff" }}>
-    <h1>NASA Media Search</h1>
+    <h1>NASA - Buscador multimedia</h1>
   </header>
 );
 
