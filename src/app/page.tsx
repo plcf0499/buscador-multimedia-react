e9a20d6e9fbecad6d19b24dd8a6c3adc87b4036c
plcf0499 <plcf0499@gmail.com>
2025-01-12 14:17:44 -0500

    v3. Buscador multimedia usando la API de la NASA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+  return (    
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">      
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">      
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -94,7 +94,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a>         
       </footer>
     </div>
   );
